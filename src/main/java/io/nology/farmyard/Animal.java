d70001610d3db8531d65ec115a323cd9ec6e0e9c
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-07-29 16:15:27 +0100

    Updated Yardm yard changes

--- src/main/java/io/nology/farmyard/Animal.java
+++ src/main/java/io/nology/farmyard/Animal.java
@@ -1,8 +1,9 @@
 package io.nology.farmyard;
 
+import java.util.Objects;
 import java.util.StringJoiner;
 
-public abstract class Animal {
+public abstract class Animal implements Comparable<Animal> {
 
     private String name;
     private int age;
@@ -38,6 +39,24 @@ public abstract class Animal {
 
     public void setLegs(int legs) {
         this.legs = legs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        return this.name.compareTo(o.name);
     }
 
     @Override
